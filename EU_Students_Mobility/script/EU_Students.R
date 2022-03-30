d2423d8cd91f73a7f0cd04dd2e51984b850732b2
adang2011 <98852244+adang2011@users.noreply.github.com>
2022-03-30 14:08:14 -0700

    finishing up tt3

--- EU_Students_Mobility/script/EU_Students.R
+++ EU_Students_Mobility/script/EU_Students.R
@@ -12,7 +12,9 @@ library(sysfonts)
 library(showtextdb)
 library(showtext)
 
+
 ## Add fonts ##################
+font_add_google("Yanone Kaffeesatz", "yanone")
 
 
 ## Load data ##################################################
@@ -20,15 +22,27 @@ Students<-read_csv(here("EU_Students_Mobility", "data", "erasmus.csv"))
 view(Students)
 
 
-## Data anlaysis ##############################################
- ## Data wrangling
+## Data analysis ##############################################
+ ## Data wrangling ###
 Students_Clean<-Students %>% 
   drop_na() %>% 
   filter(participant_gender != "Undefined") %>% 
-  select("mobility_duration", "participant_nationality", "participant_age", "participant_gender")
-
+  select("mobility_duration", "participant_nationality", "participant_age", "participant_gender", 
+         "special_needs")
 view(Students_Clean)
 
-## NOTES: -keep mobility duration, participant nationality, participant age, participant gender
-## facet gender, color  nationality, x = age and y = duration
-## try out map plots....... challenge with 3D
+  ## Data Plotting ###.
+ggplot(data = Students_Clean, 
+       aes(x = special_needs, y = mobility_duration, 
+           color = participant_gender)) +
+  geom_violin() +
+  theme(plot.title = element_text(size = 26, hjust = 0.5, family = "yanone"),
+        axis.title = element_text(size = 18, family = "yanone"), 
+        legend.title = element_text(size = 18, family = "yanone")) +
+  labs(title = "Student mobility duration based on special needs",
+       x = "Special needs (Y/N)", y = "Mobility duration", 
+       color = "Gender", 
+       caption = "Erasmus student mobility data from Data.Europa.eu")
+
+ggsave(here("EU_Students_Mobility", "output", "Nationality_based_mobility.png"), 
+       width = 3, height = 3)
