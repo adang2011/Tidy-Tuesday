864b7eac6ea1853bf8f5ee3ef9da755dda0b0ba4
adang2011 <98852244+adang2011@users.noreply.github.com>
2022-05-10 16:27:06 -0700

    adding tt8

---
+++ US_Solar_Wind/script/solar_wind.R
@@ -0,0 +1,53 @@
+#######################################################
+## Looking at Solar and Wind Price vs Capacity ########
+## Created by: Alex Dang ##############################
+#######################################################
+
+## Load libraries #####################################
+library(tidyverse)
+library(here)
+library(tvthemes)
+library(patchwork)
+
+## Load data ###########################################
+solar <- read_csv(here("US_Solar_Wind", "data", "solar.csv"))
+view(solar)
+wind <- read_csv(here("US_Solar_Wind", "data", "wind.csv"))
+view(wind)
+
+## Data analysis #######################################
+solar_plot <- solar %>% 
+  ggplot(aes(x = solar_capacity, y = solar_mwh, color = solar_capacity)) +
+  geom_jitter() +
+  scale_color_viridis_c() +
+  labs(title = "Solar Power Costs vs Capacity", 
+       x = "Solar Capacity", y = "Solar Price ($/MWh)", 
+       color = "Solar Capacity") +
+  theme(plot.title = element_text(size = 24, hjust = 0.5),
+        axis.title = element_text(size = 16), 
+        legend.title = element_text(size = 12), 
+        panel.background = element_rect(fill = "gray"), 
+        axis.text = element_text(size = 10), 
+        panel.grid = element_line(color = "beige"))
+ggsave(here("US_Solar_Wind", "output", "Solar_power_cost_cap.png"), 
+       width = 8, height = 5)
+
+wind_plot <- wind %>% 
+  ggplot(aes(x = wind_capacity, y = wind_mwh, color = wind_capacity)) +
+  geom_jitter() +
+  scale_color_viridis_c() +
+  labs(title = "Wind Power Costs vs Capacity", 
+       x = "Wind Capacity", y = "Wind Price ($/MWh)", 
+       color = "Wind Capacity") +
+  theme(plot.title = element_text(size = 24, hjust = 0.5),
+        axis.title = element_text(size = 16), 
+        legend.title = element_text(size = 12), 
+        panel.background = element_rect(fill = "gray"), 
+        axis.text = element_text(size = 10), 
+        panel.grid = element_line(color = "beige"))
+ggsave(here("US_Solar_Wind", "output", "Wind_power_cost_cap.png"), 
+       width = 8, height = 5)
+
+solar_plot/wind_plot
+ggsave(here("US_Solar_Wind", "output", "Solar_vs_Wind.png"), 
+       width = 8, height = 12)
